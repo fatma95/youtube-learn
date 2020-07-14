d35a79a2ab013546cb38598b74e68cdf418a2272
fatma95 <fatmataha0@gmail.com>
2020-07-14 16:21:29 +0200

    modified gitignore to stop tracking constants.swift

---
+++ YoutubeLearn/ConstantsTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  ConstantsTemplate.swift
+//  YoutubeLearn
+//
+//  Created by Fatma Mohamed on 7/14/20.
+//  Copyright © 2020 Fatma Mohamed. All rights reserved.
+//
+
+import Foundation
+
+/*
+struct Constants {
+    static var API_KEY = "AIzaSyCXa7KEEJ0c86QYhvFryvzMo2M5AqzZdXA"
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+
+ */
